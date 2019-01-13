372506cbb414d5320c59ec1ae85897a360314f3f
Divyanshu Tomar <tomar.divyanshu@gmail.com>
2019-01-13 22:30:04 +0530

    added google map developer version with rest markers

--- src/components/SearchPage/RestMapView/index.js
+++ src/components/SearchPage/RestMapView/index.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import GoogleMapReact from 'google-map-react';
+import RestaurantMarker from './RestaurantMarker';
 
-const RestMapView = () => {
+const restMapViewStyles = (theme) => ({
+  root: {
+    width: '100%',
+    height: '100%'
+  }
+});
+
+
+const RestMapView = ({ classes, restaurants }) => {
+  let defaultCenter = null;
+  if (restaurants.length) {
+    const { lat, lon } = restaurants[0];
+    defaultCenter = {
+      lat, lng: lon
+    };
+  }
   return (
-    <div>
-    Map View Coming Soon
+    <div className={classes.root}>
+      {defaultCenter && (
+        <GoogleMapReact
+        // bootstrapURLKeys={{ key: /*API KEY HERE */'' }}
+          defaultCenter={defaultCenter}
+          defaultZoom={14}
+        >
+          {restaurants.map((rest) => (
+            <RestaurantMarker
+              key={rest.id}
+              name={rest.title}
+              lat={rest.lat}
+              lng={rest.lon}
+            />
+          ))
+          }
+
+        </GoogleMapReact>
+      )}
     </div>
   );
 };
 
-export default RestMapView;
+export default withStyles(restMapViewStyles)(RestMapView);
